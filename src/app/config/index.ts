f5efb04843e27cbaf2650a86e2201eea84719475
Haniful Islam <hanifulislam160@gmail.com>
2026-09-03 12:47:16 +0600

    redis setup properly

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,19 +1,21 @@
 import dotenv from "dotenv";
 import path from "path";
 
-
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
-
 export default {
-    node_env: process.env.NODE_ENV,
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-    bak_url: process.env.APP_URL,
-    frontend_url: process.env.FRONTEND_URL,
-    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
-    jwt_access_secret: process.env.JWT_ACCESS_SECRET!,
-    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
-    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
-    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!
-}
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  bak_url: process.env.APP_URL,
+  frontend_url: process.env.FRONTEND_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET!,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
+  redis_username: process.env.REDIS_USERNAME,
+  redis_password: process.env.REDIS_PASSWORD,
+  redis_host: process.env.REDIS_HOST,
+  redis_port: Number(process.env.REDIS_PORT),
+};
---
+++ src/app/lib/nodemailer.ts
@@ -0,0 +1,11 @@
+// import config from "../config";
+
+// import nodemailer from "nodemailer";
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: config.smtp_user,
+//     pass: config.smtp_password,
+//   },
+// });
